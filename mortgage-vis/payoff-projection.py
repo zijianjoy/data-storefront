278517ff90b0ace7c236c1836c3b4a90930226f5
James Liu <zijianliu.joy@gmail.com>
2024-05-28 08:43:32 -0700

    add accumulated payment graph

--- mortgage-vis/payoff-projection.py
+++ mortgage-vis/payoff-projection.py
@@ -1,6 +1,8 @@
 
 import streamlit as st
 
+st.title("Mortgage Balance Projection")
+
 col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
@@ -12,21 +14,22 @@ with col2:
     st.write("Interest Rate is ", interest_rate, "%")
 
 with col3:
-    payment = st.number_input("monthly payment", value=4000)
+    payment = st.number_input("monthly payment", value=4030)
     st.write("Monthly payment ", payment, "dollars")
 
 with col4:
     original_debt = st.number_input("original_debt", value=900000)
-    st.write("The original_debt is ", original_debt, "dollars")
+    st.write("The starting debt is ", original_debt, "dollars")
 
 with col5:
     mortgage_length = st.number_input("mortgage in year", value=30)
-    st.write("The mortgage year ", mortgage_length, " years")
+    st.write("The mortgage length ", mortgage_length, " years")
 
 
 # Calculate the years which are x-axis numbers.
 x = [start_year + i for i in range(mortgage_length)]
 y = [original_debt]
+accumulated_payments = [0]
 
 # Calculate new year mortgage balance based on last year data
 for i in range(1, mortgage_length):
@@ -34,13 +37,31 @@ for i in range(1, mortgage_length):
     base = y[last_year_index]
     balance = base * (1 + (interest_rate * 0.01)) - payment * 12
     y.append(balance)
+    accumulated_payments.append(accumulated_payments[last_year_index] + payment * 12)
 
 import pandas as pd
 df = pd.DataFrame({
     "year": x,
     "balance": y,
+    "total_payments": accumulated_payments,
 })
 
 import plotly.express as px
-fig = px.bar(df, x="year", y="balance", color='balance')
+fig = px.bar(df, x="year", y="balance", color='balance', title="Year-over-year mortgage balance")
 st.plotly_chart(fig, use_container_width=True)
+
+import plotly.graph_objects as go
+
+fig_payment = go.Figure()
+fig_payment.add_trace(go.Bar(x=df["year"], y=df["total_payments"], marker=dict(
+            color=df["total_payments"] / 3,
+            colorscale="Bluered",
+            showscale=False,
+        ), name="total payments"))
+fig_payment.add_trace(go.Bar(x=df["year"], y=df["balance"], 
+                             marker=dict(color='#F4E4BA'), name="remaing balances"))
+
+# Change the bar mode
+fig_payment.update_layout(title="Accumulated payments over years", barmode='group')
+
+st.plotly_chart(fig_payment, use_container_width=True)
